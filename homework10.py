a0251fdad2a5ade8868a664d298ed0d070744be5
Gigimikava <127594709+Gigimikava@users.noreply.github.com>
2023-04-23 00:48:00 +0400

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,13 @@
+# import requests
+#
+# def get_data(URL):
+#     response=requests.get(URL)
+#     try:
+#         response.status_code == "200"
+#         print(response.content)
+#     except:
+#         print("there is an error!")
+# w1=get_data("https://api.github.com/")
+# w2=get_data("https://api.friv.com/")
+
+
